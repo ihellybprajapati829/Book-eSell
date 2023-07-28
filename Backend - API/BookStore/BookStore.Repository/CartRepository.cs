de4812a7661ffa898f5bab1f4e9f301299eb6fb0
Helly Prajapati <82837803+ihellybprajapati829@users.noreply.github.com>
2023-07-28 16:24:38 +0530

    Repositories added.

---
+++ Backend - API/BookStore/BookStore.Repository/CartRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Models.ViewModels;
+using BookStore.Models.Model;
+using System.Net;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Repository
+{
+    public class CartRepository : BaseRepository
+    {
+        /*public List<Cart> GetCartItems(string? keyword)
+        {
+            keyword = keyword?.ToLower()?.Trim();
+            var query = _context.Carts.Include(c => c.Book).Where(c => keyword == null || c.Book.Name.ToLower().Contains(keyword)).AsQueryable();
+            return query.ToList();
+        }*/
+        public ListResponse<Cart> GetCartList(int pageIndex, int pageSize, string? keyword)
+        {
+            keyword = keyword?.ToLower()?.Trim();
+            var query = _context.Carts.Include(c => c.Book).Where(c => keyword == null || c.Book.Name.ToLower().Contains(keyword)).AsQueryable();
+            int totalReocrds = query.Count();
+            List<Cart> carts = query.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+
+            return new ListResponse<Cart>()
+            {
+                Results = carts,
+                TotalRecords = totalReocrds,
+            };
+        }
+
+        public ListResponse<Cart> GetCartListall(int UserId)
+        {
+
+            var query = _context.Carts.Include(c => c.Book).Where(c => c.Userid == UserId).AsQueryable();
+
+            int totalReocrds = query.Count();
+            List<Cart> carts = query.ToList();
+
+            return new ListResponse<Cart>()
+            {
+                Results = carts,
+                TotalRecords = totalReocrds,
+            };
+        }
+
+        public Cart GetCarts(int id)
+        {
+            return _context.Carts.FirstOrDefault(c => c.Id == id);
+        }
+
+        public Cart AddCart(Cart category)
+        {
+            var entry = _context.Carts.Add(category);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public Cart UpdateCart(Cart category)
+        {
+            var entry = _context.Carts.Update(category);
+            _context.SaveChanges();
+            return entry.Entity;
+        }
+
+        public bool DeleteCart(int id)
+        {
+            var cart = _context.Carts.FirstOrDefault(c => c.Id == id);
+            if (cart == null)
+                return false;
+
+            _context.Carts.Remove(cart);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
